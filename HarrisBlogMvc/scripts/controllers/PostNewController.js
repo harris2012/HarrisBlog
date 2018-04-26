bded441c6a3d7e24cad9932dbb4fd7b62f3182cb
HarrisZhang <hc_zhang@Ctrip.com>
2018-04-26 15:13:34 +0800

    update

--- HarrisBlogMvc/scripts/controllers/PostNewController.js
+++ HarrisBlogMvc/scripts/controllers/PostNewController.js
@@ -28,10 +28,62 @@
         }
         window.searchTime = window.setTimeout(function () {
 
-            $scope.post.htmlBody = marked($scope.post.body);
-            $scope.$apply();
+            $('.md-editor-preview').html(marked($scope.post.body));
+
+            //$scope.$apply();
         }, 200)
     }
+
+    // 与CodeMirror编辑器交互 start
+    $scope.md_click_h1 = function () { this.$broadcast('CodeMirror', function (cm) { insertBefore(cm, "# ", 2); }); }//H1
+    $scope.md_click_h2 = function () { this.$broadcast('CodeMirror', function (cm) { insertBefore(cm, "## ", 3); }); }//H2
+    $scope.md_click_h3 = function () { this.$broadcast('CodeMirror', function (cm) { insertBefore(cm, "### ", 4); }); }//H3
+    $scope.md_click_h4 = function () { this.$broadcast('CodeMirror', function (cm) { insertBefore(cm, "### ", 5); }); }//H4
+    $scope.md_click_h5 = function () { this.$broadcast('CodeMirror', function (cm) { insertBefore(cm, "#### ", 6); }); }//H5
+    $scope.md_click_b = function () { this.$broadcast('CodeMirror', function (cm) { insertAround(cm, "**", "**"); }); }//加粗
+    $scope.md_click_i = function () { this.$broadcast('CodeMirror', function (cm) { insertAround(cm, "*", "*"); }); }//斜体
+    $scope.md_click_image = function () { this.$broadcast('CodeMirror', function (cm) { insertBefore(cm, "![](http://)", 2); }); }//插入图片
+    $scope.md_click_link = function () { this.$broadcast('CodeMirror', function (cm) { insertAround(cm, "[", "](http://)"); }); }//插入超链接
+    $scope.md_click_blockquote = function () { this.$broadcast('CodeMirror', function (cm) { insertBefore(cm, "> ", 2); }); }//插入超链接
+    $scope.md_click_ul = function () { this.$broadcast('CodeMirror', function (cm) { insertBefore(cm, "* ", 2); }); }//UL
+    $scope.md_click_ol = function () { this.$broadcast('CodeMirror', function (cm) { insertBefore(cm, "1. ", 3); }); }//OL
+    $scope.md_click_code = function () { this.$broadcast('CodeMirror', function (cm) { if (cm.getDoc().getSelection().indexOf("\n") > 0) { insertAround(cm, "```\n", "\n```"); } else { insertAround(cm, "`", "`"); } }); }//插入代码
+
+    function insertBefore(cm, insertion, cursorOffset) {
+        var doc = cm.getDoc();
+        var cursor = doc.getCursor();
+
+        if (doc.somethingSelected()) {
+            var selections = doc.listSelections();
+            selections.forEach(function (selection) {
+                var pos = [selection.head.line, selection.anchor.line].sort();
+
+                for (var i = pos[0]; i <= pos[1]; i++) {
+                    doc.replaceRange(insertion, { line: i, ch: 0 });
+                }
+
+                doc.setCursor({ line: pos[0], ch: cursorOffset || 0 });
+            });
+        } else {
+            doc.replaceRange(insertion, { line: cursor.line, ch: 0 });
+            doc.setCursor({ line: cursor.line, ch: cursorOffset || 0 });
+        }
+        cm.focus();
+    }
+    function insertAround(cm, start, end) {
+        var doc = cm.getDoc();
+        var cursor = doc.getCursor();
+
+        if (doc.somethingSelected()) {
+            var selection = doc.getSelection();
+            doc.replaceSelection(start + selection + end);
+        } else {
+            doc.replaceRange(start + end, { line: cursor.line, ch: cursor.ch });
+            doc.setCursor({ line: cursor.line, ch: cursor.ch + start.length });
+        }
+        cm.focus();
+    }
+    // 与CodeMirror编辑器交互 end
 
     $scope.create = function () {
 
@@ -51,7 +103,7 @@
         }
 
         {
-            var request = { };
+            var request = {};
             request.title = $scope.post.title;
             request.ename = $scope.post.ename;
             request.body = $scope.post.body;
